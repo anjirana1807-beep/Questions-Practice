a3a05abf30dca368c4234cb39f26550359352835
anjirana1807-beep <anjirana1807@gmail.com>
2026-09-14 21:50:31 +0530

    Time: 2204 ms (5.01%), Space: 7.9 MB (81.56%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        if(n<=1)
+            return 1;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans+=numTrees(i)*numTrees(n-i-1);
+        }
+        return ans;
+    }
+};
